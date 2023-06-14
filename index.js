84e7bfc4822cd09594b4b3e0031ed475d574b5a1
Collins Kariuki <collinsmunkary@gmail.com>
2023-06-14 14:15:03 +0300

    Creating products

--- index.js
+++ index.js
@@ -22,16 +22,35 @@ mongoose.connect("mongodb://127.0.0.1:27017/farmStand")
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+//middleware to parse the reqeuest body from post requests
+app.use(express.urlencoded({extended: true}))
+
 //add a basic route
 app.get('/products', async (req,res) => {
     const products = await Product.find({})
     res.render('products/index', {products})
 })
 
+//server the form to create a new product
+//we don't need to do anything asynchronous here
+app.get('/products/new', (req,res) => {
+  res.render('products/new')
+})
+
+//route where the form to create a new product will be submitted
+app.post('/products', async (req,res) => {
+  //creating the new product in MongodDB
+  const newProduct = new Product(req.body)
+  await newProduct.save();
+  //console.log(newProduct)
+  res.redirect(`/products/${newProduct._id}`)
+})
+
+
 app.get('/products/:id', async (req,res) => {
   const {id} = req.params;
   const product = await Product.findById(id);
-  console.log(product);
+  //console.log(product);
   res.render('products/show', {product})
 })
 
