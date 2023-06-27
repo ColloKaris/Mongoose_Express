5f46b606a7edbbe21a406e2df8e4fb2dba7b64ad
Collins Kariuki <collinsmunkary@gmail.com>
2023-06-27 22:12:46 +0300

    adding the product page part one

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get('/products', async (req,res) => {
     res.render('products/index', {products})
 })
 
-//server the form to create a new product
+//serve the form to create a new product
 //we don't need to do anything asynchronous here
 app.get('/products/new', (req,res) => {
   res.render('products/new')
@@ -46,13 +46,21 @@ app.post('/products', async (req,res) => {
   res.redirect(`/products/${newProduct._id}`)
 })
 
-
 app.get('/products/:id', async (req,res) => {
   const {id} = req.params;
   const product = await Product.findById(id);
   //console.log(product);
   res.render('products/show', {product})
 })
+
+//Update products
+app.get('/products/:id/edit', async (req,res) => {
+  const {id} = req.params;
+  const product = await Product.findById(id);
+  res.render('products/edit', {product})
+})
+
+
 
 
 app.listen(3000, () => {
