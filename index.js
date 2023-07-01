94fa512112a81bcd9887cfc4551b1a3c9772776a
Collins Kariuki <collinsmunkary@gmail.com>
2023-07-01 18:45:40 +0300

    Update Products route and page

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const path = require('path');
 // Connect to Mongoose
 const mongoose = require("mongoose");
+const methodOverride = require("method-override")
+
 
 //import the model created in product.js in the models folder
 const Product = require('./models/product');
@@ -25,14 +27,16 @@ app.set('view engine', 'ejs');
 //middleware to parse the reqeuest body from post requests
 app.use(express.urlencoded({extended: true}))
 
+app.use(methodOverride('_method'))
+
 //add a basic route
 app.get('/products', async (req,res) => {
     const products = await Product.find({})
     res.render('products/index', {products})
 })
 
-//serve the form to create a new product
-//we don't need to do anything asynchronous here
+// serve the form to create a new product
+// we don't need to do anything asynchronous here
 app.get('/products/new', (req,res) => {
   res.render('products/new')
 })
@@ -53,11 +57,20 @@ app.get('/products/:id', async (req,res) => {
   res.render('products/show', {product})
 })
 
-//Update products
+//Update products - gets the product we want to update
 app.get('/products/:id/edit', async (req,res) => {
   const {id} = req.params;
   const product = await Product.findById(id);
   res.render('products/edit', {product})
+})
+
+//Route to handle the updating - where we send the data to update
+app.put('/products/:id', async(req,res) => {
+  // logic to updtate the product using Mongoose
+  const {id} = req.params
+  // updating by passing the entire request body
+  const product = await Product.findByIdAndUpdate(id, req.body, {runValidators: true, new: true})
+  res.redirect(`/products/${product._id}`)
 })
 
 
